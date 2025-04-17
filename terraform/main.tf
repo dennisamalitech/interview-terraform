b10bdd5ac461f6790e9a8b9c330d671262c4db29
Dennis Machu <83217057+dennisamalitech@users.noreply.github.com>
2025-04-17 09:00:21 +0000

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_eks_cluster" "demo" {
+  name     = "rodney-eks-demo"
+  role_arn = aws_iam_role.eks_cluster.arn
+
+  vpc_config {
+    subnet_ids = [aws_subnet.public.id]  # Error 1: Missing subnet resource
+  }
+}
+
+resource "aws_iam_role" "eks_cluster" {
+  name = "eks-cluster-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "eks.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+
+# Error 2: Missing required IAM policy attachments
+# Error 3: Missing VPC/subnet resources
